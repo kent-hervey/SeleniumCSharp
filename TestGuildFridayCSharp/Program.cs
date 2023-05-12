f6e25518b6b0c8ccb556b51eb9ad84ce2cb4ceeb
Kent Hervey <kent.hervey@zealitconsultants.com>
2023-05-12 13:30:26 -0500

    this working version opens a web page, clicks a link then opens the following page

---
+++ TestGuildFridayCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace TestGuildFridayCSharp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        //!Make sure to add the path to where you are extracting the chromedriver.exe:
+
+        IWebDriver driver = new ChromeDriver(); //<-Add your path
+
+        driver.Navigate().GoToUrl("https://testguild.com/HpSupport.html");
+
+        //IWebElement myField = driver.FindElement(By.Id("tools"));
+
+        driver.FindElement(By.LinkText("QTP 11 Support Matrix with Patch Updates")).SendKeys(Keys.Enter);
+
+
+
+    }
+}
+
